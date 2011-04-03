542f40988c062e50ee16250ba0861e20827345a1
Alex Galonsky <alex@galonsky.com>
2011-04-02 23:03:55 -0400

    Root form page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var http = require('http');
+var app = express.createServer();
 
 app.configure(function() {
     app.use(express.bodyParser());
@@ -10,5 +11,9 @@ app.configure(function() {
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.render('main');
+});
 
 app.listen(4000);
