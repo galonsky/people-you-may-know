ccb43bbc271095c874faa8b81015436787fd7fcb
Alex Galonsky <alex@galonsky.com>
2011-04-02 23:24:52 -0400

    Getting user's followed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 var app = express.createServer();
+var rest = require('restler');
 
 app.configure(function() {
     app.use(express.bodyParser());
@@ -13,6 +14,12 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
+    if(req.query.username)
+    {
+        rest.get('http://github.com/api/v2/json/user/show/' + req.query.username + '/following').on('complete', function(data) {
+            console.log(data);
+        });
+    }
     res.render('main');
 });
 
